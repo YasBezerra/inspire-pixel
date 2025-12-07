184064e47ddc4855a82acab842d5deb743722d19
YasBezerra <yasmin.beatrizz@gmail.com>
2025-12-06 23:34:58 -0300

    feat: adicionando botão like

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,57 @@
 <script setup>
     defineProps(["imagem"]); 
+    import { ref } from 'vue';
+    
+    const likes = ref(0);
 
 </script>
 
 <template>
+    
     <div class="card">
     <img :src="imagem" alt="Imagens de paisagem">
+    <button @click="likes++"> {{ likes }} ❤</button>
     </div>
 </template>
 
 <style scoped lang="scss">
 
 .card {
+    position: relative;
+    transition: 0.5s ease;
+
+    .card:hover {
+        transform: translateY(-4px);
+        box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
+    }
 
     img{
+        
         width: 293px;
         height: 500px;
         border-radius: 10px;
-        display: flex;
+        display: block;
         object-fit: cover;
         gap: 10px;
+        object-fit: cover;
+    }
+
+    button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    font-size: 24px;       
+    color: white;        
+    padding: 0;
+    transition: 0.2s ease;
+    }
+
+    button:hover {
+    transform: scale(1.2); 
+    opacity: 0.9;
     }
 }
 </style>
